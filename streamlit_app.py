78bf4487cfba782bbc411c16095ab839cc097f5a
keerthi konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-02-11 21:25:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 

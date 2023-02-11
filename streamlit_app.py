e8c4fa21021250454e110a819cdfb24504f2b30a
keerthi konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-02-11 21:22:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 
 import streamlit
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
 streamlit.title('my parents')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')

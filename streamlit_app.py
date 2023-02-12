ee655210595da7da2087a588b37932fb31653f05
keerthi konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-02-12 16:17:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("pick some fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("pick some fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
